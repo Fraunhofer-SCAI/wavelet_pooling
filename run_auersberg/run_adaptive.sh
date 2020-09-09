a3286a64fce892afc7259ef9b9518052719f9395
moritz <wolter@cs.uni-bonn.de>
2020-09-09 12:07:54 +0200

    auersberg runfiles.

---
+++ run_auersberg/run_adaptive.sh
@@ -0,0 +1,3 @@
+#block(name=awp, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
+    echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
+    ipython ../train_cifar.py -- --lr 0.01 --tensorboard --pooling_type adaptive_wavelet
---
+++ run_auersberg/run_max.sh
@@ -0,0 +1,6 @@
+#block(name=mp, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
+    echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
+    ipython ../train_cifar.py -- --lr 0.01 --tensorboard --pooling_type max
+#block(name=ap, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
+    echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
+    ipython ../train_cifar.py -- --lr 0.01 --tensorboard --pooling_type max
