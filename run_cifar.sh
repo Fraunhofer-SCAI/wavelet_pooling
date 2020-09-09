213c1e8d253263743f3adf3b33f0422e124e85d6
moritz@wolter.tech <moritz@wolter.tech>
2020-09-09 11:21:08 +0200

    bundle leo scripts.

--- run_cifar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python cifar_pool.py --pool adaptive_wavelet --lr 0.1
-python cifar_pool.py --pool adaptive_wavelet --lr 0.01 --resume
-python cifar_pool.py --pool adaptive_wavelet --lr 0.001 --resume
