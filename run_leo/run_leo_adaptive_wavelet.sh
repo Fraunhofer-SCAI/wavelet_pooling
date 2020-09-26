88fb3fb6e765cf33edb5a950484d7651f7e3df0b
moritz <wolter@cs.uni-bonn.de>
2020-09-27 00:21:04 +0200

    new leo exps.

--- run_leo/run_leo_adaptive_wavelet.sh
+++ run_leo/run_leo_adaptive_wavelet.sh
@@ -7,4 +7,4 @@
 
 module load Anaconda3
 module load Singularity
-singularity exec --nv /opt/software/Singularity/pytorch-19.09-py3.sif python ../train_cifar.py --pooling_type adaptive_wavelet --lr 0.1 --tensorboard --momentum 0.4
+singularity exec --nv /opt/software/Singularity/pytorch-19.09-py3.sif python ../train_cifar.py --lr 0.01 --momentum 0.9 --b 96 --pooling_type adaptive_wavelet --tensorboard
