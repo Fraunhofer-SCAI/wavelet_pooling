e7d08615ffa280c0c2a5382c171f2737e0bc6280
moritz <wolter@cs.uni-bonn.de>
2020-09-28 18:25:31 +0200

    momentum update.

--- run_auersberg/run_cifar_max.sh
+++ run_auersberg/run_cifar_max.sh
@@ -1,3 +1,3 @@
-#block(name=mp, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
+#block(name=maxp, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../train_cifar.py -- --lr 0.03 --tensorboard --pooling_type max --momentum 0.4
+    ipython ../train_cifar.py -- --lr 0.03 --tensorboard --pooling_type max --momentum 0.0
