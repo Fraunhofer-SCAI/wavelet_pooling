e74f99ad599e3285abcf40c9109926df49b83d7c
moritz <wolter@cs.uni-bonn.de>
2020-10-01 14:01:47 +0200

    cifar loop update.

--- run_leo/cifar_loop.py
+++ run_leo/cifar_loop.py
@@ -8,40 +8,42 @@ subprocess.call('pwd')
 print('running cifar pooling sweep in parallel')
 
 jobs = []
-lr = 0.1
-momentum = 0.2
+lr = [0.05]
+momentum = [0.0, 0.3, 0.6]
 
-time_str = str(datetime.datetime.today())
-with open("p_" + 'mean' + time_str + ".txt", "w") as f:
-    jobs.append(subprocess.Popen(['python', '../train_cifar.py', 
-                                  '--pooling_type', 'max',
-                                  '--lr', str(lr),
-                                  '--momentum', str(momentum),
-                                  '--tensorboard'], stdout=f))
-with open("p_" + 'avg' + time_str + ".txt", "w") as f:
-    jobs.append(subprocess.Popen(['python', '../train_cifar.py',
-                                  '--pooling_type', 'avg',
-                                  '--lr', str(lr),
-                                  '--momentum', str(momentum),
-                                  '--tensorboard'], stdout=f))
-with open("p_" + 'wavelet' + time_str + ".txt", "w") as f:
-    jobs.append(subprocess.Popen(['python', '../train_cifar.py',
-                                  '--pooling_type', 'wavelet',
-                                  '--lr', str(lr),
-                                  '--momentum', str(momentum),
-                                  '--tensorboard'], stdout=f))
-with open("p_" + 'adaptive_wavelet' + time_str + ".txt", "w") as f:
-    jobs.append(subprocess.Popen(['python', '../train_cifar.py',
-                                  '--lr', str(lr),
-                                  '--momentum', str(momentum),
-                                  '--pooling_type', 'adaptive_wavelet',
-                                  '--tensorboard'], stdout=f))
-with open("p_" + 'scaled_wavelet' + time_str + ".txt", "w") as f:
-    jobs.append(subprocess.Popen(['python', '../train_cifar.py',
-                                  '--lr', str(lr),
-                                  '--momentum', str(momentum),
-                                  '--pooling_type', 'scaled_wavelet',
-                                  '--tensorboard'], stdout=f))
+for lr in lr_lst:
+    for momentum in momentum_lst:
+        time_str = str(datetime.datetime.today())
+        with open("p_" + 'mean' + time_str + ".txt", "w") as f:
+            jobs.append(subprocess.Popen(['python', '../train_cifar.py', 
+                                        '--pooling_type', 'max',
+                                        '--lr', str(lr),
+                                        '--momentum', str(momentum),
+                                        '--tensorboard'], stdout=f))
+        with open("p_" + 'avg' + time_str + ".txt", "w") as f:
+            jobs.append(subprocess.Popen(['python', '../train_cifar.py',
+                                        '--pooling_type', 'avg',
+                                        '--lr', str(lr),
+                                        '--momentum', str(momentum),
+                                        '--tensorboard'], stdout=f))
+        with open("p_" + 'wavelet' + time_str + ".txt", "w") as f:
+            jobs.append(subprocess.Popen(['python', '../train_cifar.py',
+                                        '--pooling_type', 'wavelet',
+                                        '--lr', str(lr),
+                                        '--momentum', str(momentum),
+                                        '--tensorboard'], stdout=f))
+        with open("p_" + 'adaptive_wavelet' + time_str + ".txt", "w") as f:
+            jobs.append(subprocess.Popen(['python', '../train_cifar.py',
+                                        '--lr', str(lr),
+                                        '--momentum', str(momentum),
+                                        '--pooling_type', 'adaptive_wavelet',
+                                        '--tensorboard'], stdout=f))
+        with open("p_" + 'scaled_wavelet' + time_str + ".txt", "w") as f:
+            jobs.append(subprocess.Popen(['python', '../train_cifar.py',
+                                        '--lr', str(lr),
+                                        '--momentum', str(momentum),
+                                        '--pooling_type', 'scaled_wavelet',
+                                        '--tensorboard'], stdout=f))
 
 for job in jobs:
     job.wait()
