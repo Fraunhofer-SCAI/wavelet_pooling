5d9f02ff70f9c68e0e26d1b203490bd74d88fa5f
moritz <wolter@cs.uni-bonn.de>
2020-10-11 19:34:54 +0200

    bugfix.

--- run_auersberg/run_cifar_max.sh
+++ run_auersberg/run_cifar_max.sh
@@ -1,3 +1,3 @@
 #block(name=maxp, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../train_cifar.py -- --lr 0.1 --tensorboard --pooling_type max --momentum 0.9 --model DenseNet
+    ipython ../train_cifar_SVHN.py -- --lr 0.1 --tensorboard --pooling_type max --momentum 0.9 --model DenseNet
