d74c685a26a329d8c0c2197eb409bd72f5ffa28d
moritz <wolter@cs.uni-bonn.de>
2020-10-12 21:26:21 +0200

    cifar densenet reruns.

--- bear_img_wave_opt.py
+++ bear_img_wave_opt.py
@@ -3,13 +3,13 @@ import numpy as np
 import torch
 from util.conv_transform import conv_fwt_2d, conv_ifwt_2d, get_pad
 from util.conv_transform import flatten_2d_coeff_lst
-from util.learnable_wavelets import SoftOrthogonalWavelet
+from util.learnable_wavelets import ProductFilter
 from scipy import misc
 import matplotlib.pyplot as plt
 import tikzplotlib
 
 scales=5
-iterations = 250
+iterations = 1500
 face = misc.face()  # [128:(512+128), 256:(512+256)]
 face = face / 255.
 face = torch.tensor(face.astype(np.float32))
@@ -44,7 +44,7 @@ def set_scale(wavelet_coeffs, zero_scale=5, weights:list=None):
     return coefficients_low
 
 
-wavelet = SoftOrthogonalWavelet(  # ProductFilter(
+wavelet = ProductFilter(
             torch.tensor([0, 0, 0.7071067811865476, 0.7071067811865476, 0, 0],
                          requires_grad=True),
             torch.tensor([0, 0, -0.7071067811865476, 0.7071067811865476, 0, 0],
@@ -57,13 +57,13 @@ wavelet = SoftOrthogonalWavelet(  # ProductFilter(
 scale_weights = torch.ones(size=[scales], requires_grad=True) # + torch.rand([scales])
 
 # opt = torch.optim.Adagrad(list(wavelet.parameters()) + [scale_weights], lr=0.001)
-opt = torch.optim.Adam(list(wavelet.parameters()) + [scale_weights], lr=0.001)
+opt = torch.optim.Adam(list(wavelet.parameters()) + [scale_weights], lr=1e-03)
 coeffs = conv_fwt_2d(face, wavelet, scales=scales)
 z_coeffs = set_scale(coeffs, zero_scale=scales-1, weights=scale_weights)
 init_rec = conv_ifwt_2d(z_coeffs, wavelet)
 down_face = init_rec/torch.max(torch.abs(init_rec))
-plt.imshow(down_face[:, 0, :, :].detach().permute([1, 2, 0]).numpy())
-plt.show()
+# plt.imshow(down_face[:, 0, :, :].detach().permute([1, 2, 0]).numpy())
+# plt.show()
 
 mse_list = []
 
@@ -73,7 +73,7 @@ for i in range(iterations):
     z_coeffs = set_scale(coeffs, zero_scale=scales-1, weights=scale_weights)
     rec = conv_ifwt_2d(z_coeffs, wavelet)
     diff = rec - face
-    mse = torch.sum(diff*diff) + wavelet.wavelet_loss()*10000.
+    mse = torch.sum(diff*diff) + wavelet.wavelet_loss()
     mse.backward()
     opt.step()
     print(i,
@@ -82,8 +82,9 @@ for i in range(iterations):
     mse_list.append(mse.detach().numpy())
 
 
+plt.figure()
 plt.plot(mse_list)
-# tikzplotlib.save('opt_mse.tex', standalone=True)
+tikzplotlib.save('opt_mse.tex', standalone=True)
 plt.show()
 
 diff_s = (init_rec - rec)*(init_rec - rec)
@@ -98,17 +99,17 @@ print('optimization finished')
 plt.figure()
 plt.imshow(final[:, 0, :, :].permute([1, 2, 0]).detach().numpy())
 plt.title('optimized-haar')
-# tikzplotlib.save('optimized_haar.tex', standalone=True)
+tikzplotlib.save('optimized_haar.tex', standalone=True)
 plt.show()
 plt.figure()
 plt.imshow(down_face[:, 0, :, :].permute([1, 2, 0]).detach().numpy())
 plt.title('haar')
-# tikzplotlib.save('haar.tex', standalone=True)
+tikzplotlib.save('haar.tex', standalone=True)
 plt.show()
 plt.figure()
-plt.imshow(diff_s[:, 0, :, :].permute([1, 2, 0]).detach().numpy())
+plt.imshow(torch.mean(diff_s[:, 0, :, :], 0).detach().numpy(), cmap='Greys')
 plt.title('normalized-difference')
-# tikzplotlib.save('normalized_difference.tex', standalone=True)
+tikzplotlib.save('normalized_difference.tex', standalone=True)
 plt.show()
 
 print('done')
