1f37377222ecfd97753e4a45afa93aacff29cc10
moritz <wolter@cs.uni-bonn.de>
2020-10-13 12:30:13 +0200

    fix weight decay.

--- run_auersberg/run_cifar_fixed.sh
+++ run_auersberg/run_cifar_fixed.sh
@@ -1,3 +1,3 @@
 #block(name=fwp, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../train_cifar_SVHN.py -- --lr 0.05 --tensorboard --pooling_type wavelet --momentum 0.9  --model DenseNet --nesterov
+    ipython ../train_cifar_SVHN.py -- --lr 0.05 --tensorboard --pooling_type wavelet --momentum 0.9  --model DenseNet --nesterov --wd 1e-4
