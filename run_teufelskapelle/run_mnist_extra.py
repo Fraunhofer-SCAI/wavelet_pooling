7d4b68a1ccaa1acdf428a481a197e755863c479a
moritz@wolter.tech <moritz@wolter.tech>
2020-11-30 20:04:06 +0100

    fix jobs.

--- run_teufelskapelle/run_mnist_extra.py
+++ run_teufelskapelle/run_mnist_extra.py
@@ -1,21 +1,21 @@
 import subprocess
 subprocess.call('pwd')
 
-print('running mnist parameter sweep in parallel')
+print('running mnist extra rebuttal exps.')
 
 for exp in range(10):
     job = subprocess.Popen(['python', '../mnist_pool.py',
-                            '--lr 0.12', '--tensorboard',
-                            '--pooling_type max',
-                            '--momentum 0.6',
-                            '--gamma 0.95', '--epochs 25'])
+                            '--lr', '0.12', '--tensorboard',
+                            '--pooling_type', 'max',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
     job.wait()
 
 
 for exp in range(10):
     job = subprocess.Popen(['python', '../mnist_pool.py',
-                            '--lr 0.12', '--tensorboard',
-                            '--pooling_type mean',
-                            '--momentum 0.6',
-                            '--gamma 0.95', '--epochs 25'])
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'mean',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
     job.wait()
