6fb80427e3f739d5004f7c010814e3556f8d8210
moritz@wolter.tech <moritz@wolter.tech>
2020-11-30 19:38:34 +0100

    exp.

--- run_auersberg/run_mnist_extra.sh
+++ run_auersberg/run_mnist_extra.sh
@@ -1,30 +1,30 @@
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
 #block(name=mnist_extra, threads=2, memory=7500, subtasks=1, hours=68, gpus=1)
     echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type adaptive_wavelet --momentum 0.6 --gamma 0.95 --epochs 25
+    ipython ../mnist_pool.py -- --lr 0.12 --tensorboard --pooling_type max --momentum 0.6 --gamma 0.95 --epochs 25
