89b8d691212584a005a7790094a1b21ddf406869
moritz@wolter.tech <moritz@wolter.tech>
2020-11-30 20:23:34 +0100

    exp code.

--- run_teufelskapelle/run_mnist_extra.py
+++ run_teufelskapelle/run_mnist_extra.py
@@ -11,11 +11,34 @@ for exp in range(10):
                             '--gamma', '0.95', '--epochs', '25'])
     job.wait()
 
-
 for exp in range(10):
     job = subprocess.Popen(['python', '../mnist_pool.py',
                             '--lr',  '0.12', '--tensorboard',
                             '--pooling_type', 'mean',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
+    job.wait()
+
+for exp in range(10):
+    job = subprocess.Popen(['python', '../mnist_pool.py',
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'scaled_wavelet',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
+    job.wait()
+
+for exp in range(10):
+    job = subprocess.Popen(['python', '../mnist_pool.py',
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'wavelet',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
+    job.wait()
+
+for exp in range(10):
+    job = subprocess.Popen(['python', '../mnist_pool.py',
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'adaptive_wavelet',
                             '--momentum', '0.6',
                             '--gamma', '0.95', '--epochs', '25'])
     job.wait()
