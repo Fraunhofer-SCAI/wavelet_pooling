435e51c781968058dbbfb7744b4779c10305c3f9
moritz@wolter.tech <moritz@wolter.tech>
2021-02-18 16:37:54 +0100

    add new experiments.

--- experiments/run_mnist.py
+++ experiments/run_mnist.py
@@ -3,6 +3,31 @@ subprocess.call('pwd')
 
 print('running mnist extra rebuttal exps.')
 
+
+for exp in range(10):
+    job = subprocess.Popen(['python', '../mnist_pool.py',
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'adaptive_wavelet',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
+    job.wait()
+
+for exp in range(10):
+    job = subprocess.Popen(['python', '../mnist_pool.py',
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'adaptive_max',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
+    job.wait()
+
+for exp in range(10):
+    job = subprocess.Popen(['python', '../mnist_pool.py',
+                            '--lr',  '0.12', '--tensorboard',
+                            '--pooling_type', 'adaptive_avg',
+                            '--momentum', '0.6',
+                            '--gamma', '0.95', '--epochs', '25'])
+    job.wait()
+
 for exp in range(10):
     job = subprocess.Popen(['python', '../mnist_pool.py',
                             '--lr', '0.12', '--tensorboard',
@@ -31,14 +56,6 @@ for exp in range(10):
     job = subprocess.Popen(['python', '../mnist_pool.py',
                             '--lr',  '0.12', '--tensorboard',
                             '--pooling_type', 'wavelet',
-                            '--momentum', '0.6',
-                            '--gamma', '0.95', '--epochs', '25'])
-    job.wait()
-
-for exp in range(10):
-    job = subprocess.Popen(['python', '../mnist_pool.py',
-                            '--lr',  '0.12', '--tensorboard',
-                            '--pooling_type', 'adaptive_wavelet',
                             '--momentum', '0.6',
                             '--gamma', '0.95', '--epochs', '25'])
     job.wait()
